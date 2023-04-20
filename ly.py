2072127efd47c1e968e4db3c3186ce4075112934
Wyatt <wyattcannon78@gmail.com>
2023-04-20 12:55:46 -0700

    Added tie consolidation

--- ly.py
+++ ly.py
@@ -3,7 +3,7 @@ from re import Match
 from fractions import Fraction
 import subprocess
 import os
-noter = re.compile("(?P<name>[a-z,']*)(?:(?P<durint>[0-9]))?(?:(?P<durdots>[\.]*))?(?:(?P<tie>~?))?")
+noter = re.compile("(?P<name>(?!\\\\)[abcdefgsi,']+)(?:(?P<durint>[0-9]))?(?:(?P<durdots>[\.]*))?(?:(?P<tie>~?))?")
 barliner = re.compile("\|")
 timesignaturer = re.compile("\\\\time (?P<numerator>[0-9]*)\/(?P<denominator>[0-9]*)")
 def find_notes(string):
@@ -17,8 +17,8 @@ def find_timesignatures(string):
     timesignatures = timesignaturer.finditer(string)
     return [b for b in timesignatures]
 
-def get_dur_as_frac(note: Match):
-    if(note.group("durint")):
+def get_dur_as_frac(note: Match, string):
+    def has_dur(note):
         i = int(note.group("durint"))
         dots = note.group("durdots")
         value = Fraction(1, i)
@@ -27,8 +27,23 @@ def get_dur_as_frac(note: Match):
         for d in range(0, n_dots):
             value_with_dots += Fraction(1, (d+1)*(i*2))
         return value_with_dots
-    else: return None
-    
+    if(note.group("durint")):
+        return has_dur(note)
+    else:
+        notes = find_notes(string[:note.start()])
+        for n in range(len(notes), 0):
+            if(n.group("durint")):
+                return has_dur(note)
+        return Fraction(1,4)
+def get_dur_from_frac(fraction):
+    remainder = fraction%1
+    if(remainder.numerator == 3):
+        return f"{fraction.denominator}."
+    if(remainder.numerator == 1):
+        return f"{fraction.denominator}"
+    if(remainder.numerator == 7):
+        return f"{remainder.denominatoor}.."
+    else: return "1"
 def insert_at_index(string, index, item):
     return string[:index]+item+string[index:]
 def remove_at_index(string, start, end):
@@ -38,13 +53,11 @@ def add_barlines(string, bars):
     note_n = 0
     inserted = 0
     new_string = string
-    current_dur = Fraction(1, 4)
     for b in bars:
         total = 0
         while(total < b and note_n < len(notes)):
             note = notes[note_n]
-            note_val = get_dur_as_frac(notes[note_n]) or current_dur
-            current_dur = note_val
+            note_val = get_dur_as_frac(notes[note_n], new_string)
             total += note_val
             if(total == b):
                 barline = " |"
@@ -75,11 +88,30 @@ def add_timesignatures(string, bars):
             inserted += len(timesig)
             current_time = bars[i+1]
     return new_string
+def consolidate_ties(string):
+    tier = re.compile("(\w+~+\s+)+(?:([abcdefgis0-9,\.']+))?")
+    new_string = string
+    tiechains = tier.finditer(string)
+    inserted = 0
+    for t in tiechains:
+        total_l = 0
+        notes = find_notes(t.group())
+        for n in notes:
+            total_l += get_dur_as_frac(n, new_string)
+        total_dur = get_dur_from_frac(total_l)
+
+        insert_note = notes[0].group("name")+total_dur+notes[-1].group("tie")+" "
+        new_string = insert_at_index(new_string, inserted+t.end(), insert_note)
+        new_string = remove_at_index(new_string, t.start()+inserted, t.end()+inserted)
+        inserted += len(insert_note)-len(t.group())
+    return new_string
+    # Returns tie groups
 def run_ly(string, outDir, filename):
     outputFile = os.path.join(outDir, filename)
     with open(outputFile, "w") as lyfile:
         lyfile.write(string)
     subprocess.run(["lilypond", "-o", outDir, outputFile], shell=True)
 test_bars =[ Fraction(4,4), Fraction(4,4), Fraction(3, 4), Fraction(2,4)]
-test_expression = "{ a4 b c d e f g a b c d e f g a b c }"
-run_ly(add_timesignatures(add_barlines(test_expression, test_bars), test_bars), "./output", "test.ly")
+test_expression = "{ a4~ a~ a~ a~ a~ a e f g a~ a b c~ c~ c b c d e f g a b c } "
+run_ly(
+    consolidate_ties(add_timesignatures(add_barlines(test_expression, test_bars), test_bars)), "./output", "test.ly")
